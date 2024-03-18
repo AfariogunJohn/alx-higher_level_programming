afac3c832fbedd2c65035fa450815073bc90178a
Afariogun John <Afariogunjohn2002@gmail.com>
2024-03-18 14:30:20 -0700

    testing

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""a function thatchecks for instaces"""
+
+
+def is_same_class(obj, a_class):
+    return type(obj) is a_class
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""a function thatchecks for instaces"""
+
+
+def is_kind_of_class(obj, a_class):
+    return (isinstance(obj, a_class))
